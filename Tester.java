93c7feb4d157e0430849ef07c6999ef95ddce36e
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-11-03 23:38:20 -0500

    tested set method and get method

--- Tester.java
+++ Tester.java
@@ -5,7 +5,14 @@ public class Tester{
     words.add("kani");
     words.add("uni");
     words.add("ebi");
+    // Test size function
     System.out.println(  "Size is " + words.size());
+    // Test get function
+    for(int i = 0; i < words.size(); i++){
+      System.out.println( words.get(i) );
+    }
+    // Test set function
+    System.out.println( words.set(2, "replacement") );
     for(int i = 0; i < words.size(); i++){
       System.out.println( words.get(i) );
     }
