22589b6988dcab5b2c3cc337b26b2dd3181423d9
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-11-04 00:00:03 -0500

    minor changes

--- SuperArray.java
+++ SuperArray.java
@@ -1,34 +1,32 @@
 public class SuperArray{
   private String[] data;
   private int size;
-  private int index;
 
   public SuperArray(){
     this.data = new String[10];
-    this.size = 10;
-    this.index = 0;
+    this.size = 0;
   }
 
   public int size(){
-    return this.index;
+    return this.size;
   }
 
   public boolean add(String _element){
-    if(this.index+1 == size)
+    if(this.size == data.length)
       resize();
-    this.data[this.index] = _element;
-    this.index++;
+    this.data[this.size] = _element;
+    this.size++;
     return true;
   }
 
   public String get(int _index){
-    if(_index > size)
+    if(_index >= size)
       return "";
     return this.data[_index];
   }
 
   public String set(int _index, String _element){
-    if(_index > size)
+    if(_index > data.length)
       return "";
     String s = this.data[_index];
     this.data[_index] = _element;
@@ -36,12 +34,11 @@ public class SuperArray{
   }
 
   private void resize(){
-    String[] newArray = new String[size+10];
-    for(int i=0; i<size; i++){
+    String[] newArray = new String[data.length+10];
+    for(int i=0; i<data.length; i++){
       newArray[i] = this.data[i];
     }
     this.data = newArray;
-    size += 10;
   }
 
 }
