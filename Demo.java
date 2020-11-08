3f647454178cbe5b607a18c5cac0f66ec02a92af
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-11-08 15:33:20 -0500

    Added findOverlap

--- Demo.java
+++ Demo.java
@@ -10,6 +10,17 @@ public class Demo{
       }
     }
   }
+  public static SuperArray findOverlap(SuperArray _a, SuperArray _b){
+    int s = _a.size();
+    SuperArray overlap = new SuperArray();
+    for(int i=0; i<s; i++){
+      if(_b.contains(_a.get(i)))
+        overlap.add(_a.get(i));
+    }
+    removeDuplicates(overlap);
+    return overlap;
+  }
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -19,5 +30,23 @@ public class Demo{
 
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+
+    SuperArray a = new SuperArray();
+    SuperArray b = new SuperArray();
+    a.add("9");
+    a.add("1");
+    a.add("2");
+    a.add("2");
+    a.add("3");
+    a.add("4");
+
+    b.add("0");
+    b.add("4");
+    b.add("2");
+    b.add("2");
+    b.add("9");
+
+    System.out.println(findOverlap(a,b).toString());
+  }
 }
